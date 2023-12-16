c654be939621288da3ddcd00be84238b0689a237
Tamanna Shermin <tshermin201130@bscse.uiu.ac.bd>
2023-12-16 18:51:06 +0600

    questions folder structure created

--- src/pages/Loginpage.js
+++ src/pages/Loginpage.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Form, FormGroup, FormControl, Button, Alert } from "react-bootstrap";
 import StudentImage from "../templates/Base/images/student.jpg";
-import LoginImage from "../templates/Base/images/Capture.png"
+import LoginImage from "../templates/Base/images/Capture.PNG"
 
 import { login } from "../stores/reducers/userInfo";
 import { useDispatch } from "react-redux";
